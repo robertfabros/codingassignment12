3678403a121d7b8483358e6b5d086e7843ae9af3
“Robert <robert@robertfabros.com>
2024-07-10 10:15:58 -0500

    Transitioning back to success test

--- src/components/MyButton/Button.test.tsx
+++ src/components/MyButton/Button.test.tsx
@@ -9,6 +9,6 @@ describe('MyButton Component', () => {
     const buttonElement = screen.getByText(/Primary Button/i);
     expect(buttonElement).toBeInTheDocument();
     expect(buttonElement).toHaveStyle({ backgroundColor: '#1e90ff' });
-    expect(buttonElement).toHaveTextContent('Third');
+    expect(buttonElement).toHaveTextContent('Primary Button');
   });
 });
