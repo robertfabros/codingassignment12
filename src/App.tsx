93ca676692a6adecdf5ebc33a5b24181013eaac3
“Robert <robert@robertfabros.com>
2024-07-26 16:37:38 -0500

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Route, Routes } from 'react-router-dom';
 import TopNav from './components/TopNav/TopNav';
 import Header from './components/Header';
 import Hero from './components/Hero/Hero';
@@ -11,8 +11,20 @@ import FrontEndDev from './components/FrontEndDev';
 import ContactMe from './components/ContactMe';
 import Section from './components/Section/Section';
 
+// Function to determine which router to use
+const getRouter = () => {
+  // Check the hostname or any other environment-specific variable
+  if (window.location.hostname === 'robertfabros.github.io') {
+    return HashRouter;
+  } else {
+    return BrowserRouter;
+  }
+};
 
+// Use the Router directly in the component
 const App: React.FC = () => {
+  const Router = getRouter();
+
   return (
     <Router>
       <Section className="main-section">
