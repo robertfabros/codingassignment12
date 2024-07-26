95bb5945e169702581304baf0f131548482e03ec
“Robert <robert@robertfabros.com>
2024-07-26 06:24:07 -0500

    Fixed the errors

--- src/components/Hero/Hero.test.tsx
+++ src/components/Hero/Hero.test.tsx
@@ -1,12 +1,16 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Hero from './Hero';
 
 describe('Hero Component', () => {
-
   test('renders Hero component with title and subtitle', () => {
-    const { getByText } = render(<Hero />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Hero />
+      </MemoryRouter>
+    );
     const titleElement = getByText(/Hello. My name is Robert and I am a/i);
     const subtitleElement = getByText(/Front-End Dev & Graphic Designer/i);
 
@@ -17,9 +21,11 @@ describe('Hero Component', () => {
   test('renders children content', () => {
     const childrenContent = 'Test Children Content';
     const { getByText } = render(
-      <Hero>
-        <p>{childrenContent}</p>
-      </Hero>
+      <MemoryRouter>
+        <Hero>
+          <p>{childrenContent}</p>
+        </Hero>
+      </MemoryRouter>
     );
 
     const childrenElement = getByText(childrenContent);
