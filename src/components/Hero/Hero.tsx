da0f8a9c27136319bae6af8344292aef6736bd55
“Robert <robert@robertfabros.com>
2024-07-26 06:09:25 -0500

    Added links to buttons

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Text from '../Text/Text';
 import MyButton from '../MyButton/MyButton';
 import Image from '../Image/Image';
+import { Link } from 'react-router-dom';
 
 const Hero: React.FC = ({ children }) => (
   <section id="hero">
@@ -9,8 +10,12 @@ const Hero: React.FC = ({ children }) => (
       <Text className="text-wrapper" content="Hello. My name is Robert and I am a" tag="h1" />
       <Text className="front-end-dev" content="Front-End Dev & Graphic Designer" tag="h2" />
       <div className="buttons_main">
-        <MyButton label="SERVICES" className="myServices" style={{ cursor: 'pointer' }} />
-        <MyButton label="CONTACT ME" className="contact_me" style={{ cursor: 'pointer' }} />
+        <Link to="/front-end-dev">
+          <MyButton label="SERVICES" className="myServices" style={{ cursor: 'pointer' }} />
+        </Link>
+        <Link to="/contact-me">
+          <MyButton label="CONTACT ME" className="contact_me" style={{ cursor: 'pointer' }} />
+        </Link>
       </div>
     </div>
     <div className="frame_2">
